a72bcac37b9b6fe39d702b7542205fdcf277ccd5
sasu <sasuolander@yahoo.com>
2017-09-24 20:48:19 +0300

    Creating separate itemRowMapperView for general viewing

---
+++ src/main/java/dao/ItemRowMapperView.java
@@ -0,0 +1,23 @@
+package dao;
+
+import object.Item;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.springframework.jdbc.core.RowMapper;
+
+//This RowMapper is used when printing information to view. 
+//SELECT Item_name,UT.Username,Price, T_timetamp FROM shared_burden AS SB
+//JOIN userTable AS UT  ON  SB.User_ID = UT.User_ID;
+
+public class ItemRowMapperView  implements RowMapper<Item>{
+	
+	public Item mapRow(ResultSet rs,int rowNum) throws SQLException{
+		Item i = new Item();
+		i.setId(rs.getInt("Item_id"));
+		i.setUsername(rs.getString("Username"));
+		i.setName(rs.getString("Item_Name"));
+		i.setTimestamp(rs.getDate("T_timestamp"));
+		i.setPrice(rs.getDouble("Price"));
+		return i;
+	}	
+}
