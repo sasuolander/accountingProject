88e4eae104f174266a2959705ad152ec9beb8a21
sasu <sasuolander@yahoo.com>
2017-09-28 13:20:54 +0300

    adding controller

---
+++ src/main/java/controller/ItemController.java
@@ -0,0 +1,12 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+@Controller
+public class ItemController {
+
+}
---
+++ src/main/java/controller/RestAuthenticationEntryPoint.java
@@ -0,0 +1,20 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+
+@Component("resAuthenticationEntryPoint")
+public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
+	
+ public void commence( HttpServletRequest request, HttpServletResponse response,
+ AuthenticationException authException ) throws IOException{
+ response.sendError( HttpServletResponse.SC_UNAUTHORIZED, “Unauthorized” );
+ }
+}
