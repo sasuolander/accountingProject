599feb1081f80ab4e5a6075cf73abf45f622539a
sasu <sasuolander@yahoo.com>
2017-09-28 13:21:33 +0300

    --

--- src/main/java/controller/ItemController.java
+++ src/main/java/controller/ItemController.java
@@ -6,7 +6,10 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+
 @Controller
+@RequestMapping()
 public class ItemController {
 
 }
