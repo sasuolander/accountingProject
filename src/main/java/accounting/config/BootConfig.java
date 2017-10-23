935be7dae926d08e6ce9e5196f4c9b0d94afd056
sasu <sasuolander@yahoo.com>
2017-10-23 08:46:12 +0300

    Creating configuration class and excluding database autoconfiguration

---
+++ src/main/java/accounting/config/BootConfig.java
@@ -0,0 +1,11 @@
+package accounting.config;
+
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.boot.autoconfigure.jdbc.*;
+import org.springframework.context.annotation.*;
+
+@Configuration
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+public class BootConfig {
+
+}
